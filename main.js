57e0aecec38a10124dd9a920c630735f27db4050
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-11-17 15:29:03 +0100

    btn

---
+++ main.js
@@ -0,0 +1,35 @@
+
+/* L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
+
+const button = document.getElementById('btn');
+
+
+
+button.addEventListener("click",
+
+    function onClick(){
+
+            const container = document.querySelector('.container');
+            
+            const element = document.createElement('div');
+            element.classList.add('square')
+
+    }
+
+)
+
+
+
+
+
+
+
+
+//  definisco la funzione per creare i blocchi 
+
+
+
+    
